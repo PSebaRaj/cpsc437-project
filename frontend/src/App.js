2e58dfcf0260e9b8fbd944bd02ec091b866e5d1c
Nikhil Ismail <nikhil.ismail20@gmail.com>
2023-11-22 23:22:22 -0500

    setting up barebones react app

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CPSC 437 Final Project - Patrick Sebaraj, Nikhil Ismail, Andy Yang
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
